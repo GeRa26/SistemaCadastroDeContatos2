7c11d861f3da8dd8ab00bbc15e2dd68116e39b9e
Geraldo Neto <geraldospn26@gmail.com>
2025-12-01 13:29:04 -0300

    [BACKEND] FEAT: adicionando Model de Contato e classe BancoContext

---
+++ SistemaCadastroDeContatos2/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaCadastroDeContatos2.Models;
+
+namespace SistemaCadastroDeContatos2.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
